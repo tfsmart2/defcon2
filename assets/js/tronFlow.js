a2b91d963c5714a273bd78c1f1597a492377a044
tfsmart2 <74022167+tfsmart2@users.noreply.github.com>
2020-11-25 00:23:46 +0100

    Update tronFlow.js

--- assets/js/tronFlow.js
+++ assets/js/tronFlow.js
@@ -357,7 +357,7 @@ let invester = await contract.players(currentAccount).call();
   const statsreinves = (parsefloat(invested)-parsefloat(amountuser)+parsefloat((payout / 4))-parsefloat(refrewards))*2;
  // $('#statsreinvest').text(statsreinvest);
   
-  $('#statsreinvest').text(statsreinves);
+  $('#statsreinvest').text(3);
   
 
 
