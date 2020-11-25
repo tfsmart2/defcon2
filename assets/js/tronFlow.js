d18c15e8c360a7b483a874a44c37aa2425e9d63a
tfsmart2 <74022167+tfsmart2@users.noreply.github.com>
2020-11-25 18:18:16 +0100

    Update tronFlow.js

--- assets/js/tronFlow.js
+++ assets/js/tronFlow.js
@@ -165,6 +165,7 @@ $(document).ready(async () => {
         totalProfit = (profit.toNumber() / 1000000).toFixed(6);
         halfProfit = (profit.toNumber() / 2000000).toFixed(6);
         statstotalprof = (profit.toNumber() / 1000000).toFixed(6);
+        $('#statstotalprof').text(statstotalprof);
         
 
         $('#refererAddress').val('You Already have a Sponsor');
@@ -199,7 +200,7 @@ $(document).ready(async () => {
         $('#withdrawableAmount').val(halfProfit);
         $('#withdrawableInterest').val(halfProfit);
         $('#totalWithdrawable').val(totalProfit);
-        $('#statstotalprof').val(statstotalprof);
+        
         
       }
       $('.deduction').text(halfProfit);
